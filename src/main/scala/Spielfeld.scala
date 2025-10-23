56efeb93578b5d5485a638c90b7670674967f494
spaghett1 <b_alexander1@web.de>
2025-10-23 12:07:37 +0200

    Initial commit

---
+++ src/main/scala/Spielfeld.scala
@@ -0,0 +1,16 @@
+import scala.io.StdIn._
+@main def Spielfeld(): Unit =
+  print("Spieleranzahl: ")
+  val playerCount = readInt()
+  val deckSize = 32 / playerCount
+  var newdeckSize = 0
+
+  if (deckSize < 8)
+    newdeckSize = deckSize * (playerCount / 4)
+  else
+    newdeckSize = deckSize
+
+  println(newdeckSize)
+
+  
+
