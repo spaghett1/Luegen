b49c65adf3355aa74afe94e361796084283d2c90
spaghett1 <b_alexander1@web.de>
2025-10-28 14:02:36 +0100

    class Player added

--- src/main/scala/Spielfeld.scala
+++ src/main/scala/Spielfeld.scala
@@ -1,7 +1,42 @@
 import scala.io.StdIn._
+
+case class Player(name: String, hand: List[String] = List.empty, var score: Int = 0, var lives: Int = 3)
+
+val players = playerName.map(name => Player(name))
+
+def getScore(players: Seq[Player]): Map[String, Int] =
+  players.map(p => p.name -> p.score).toMap
+
+def addscore(players: Seq[Player], playerName: String, scoreToAdd: Int): Unit =
+  players.find(_.name == playerName).foreach(_.score += scoreToAdd)
+
+
+
 @main def Spielfeld(): Unit =
-  print("Spieleranzahl: ")
-  val playerCount = readInt()
+  println("Wilkommen bei Lügen!!!")
+
+  // Spieleranzahl
+  var playerCount = 0
+  while playerCount < 2 || playerCount > 8 do
+    print("Spieleranzahl (2-8): ")
+    try 
+      playerCount = readInt()
+      if playerCount < 2 || playerCount > 8 then
+        println("Bitte Spieleranzahl neu eingenen(2-8):")
+        catch
+          case _: NumberFormatException =>
+            println("Ungültige Eingabe. Bitte eine Zahl eingeben.")
+
+  // Spieler Namen abfragen
+  val playerName = for i <- 1 to playerCount yield
+    print(s"Name Spieler $i: ")
+    readLine().trim
+  
+  // Spielerübersicht ausgeben
+  println("\nSpielerübersicht:")
+  for i <- 0 until playerCount do
+    println(s"Spieler ${i + 1}: ${playerName(i)}")
+
   val deckSize = 32 / playerCount
   var newdeckSize = 0
 
@@ -9,11 +44,9 @@ import scala.io.StdIn._
     newdeckSize = deckSize * (playerCount / 4)
   else
     newdeckSize = deckSize * 4
-    println("test")
-    println("hallo")
 
+  println(s"Deckgröße: $newdeckSize")
 
-  println(newdeckSize)
 
   
 
